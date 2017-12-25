751c7a67598ebc2e97f62974ee45a969a5bf7992
kenju-wagatsuma <kenju-wagatsuma@cookpad.com>
2017-12-25 22:10:30 +0900

    add weighted moving average

--- lib/moving_avg.rb
+++ lib/moving_avg.rb
@@ -1,14 +1,29 @@
 require "moving_avg/version"
 
+require "pry"
+
 module MovingAvg
   class Base
     class << self
       # SMA - Simple Moving Average
       def simple_moving_average(u)
-        total = u.reduce(0) { |x, y| x + y }
-        total.to_f / u.length
+        sum_vector(u).to_f / u.size
       end
       alias_method :sma, :simple_moving_average
+
+      # WMA - Weighted Moving Average
+      def weighted_moving_average(u)
+        weights = (1..u.size).to_a.reverse # [n, n-1, n-2 ... 2, 1]
+        weighted = weights.zip(u).map { |w, x| w * x  }
+        sum_vector(weighted).to_f / sum_vector(weights)
+      end
+      alias_method :wma, :weighted_moving_average
+
+      private
+
+      def sum_vector(u)
+        u.reduce(0) { |x, y| x + y }
+      end
     end
   end
 end
--- spec/moving_avg_spec.rb
+++ spec/moving_avg_spec.rb
@@ -11,4 +11,13 @@ RSpec.describe MovingAvg do
       expect(actual).to eq expected
     end
   end
+
+  describe "weighted_moving_average" do
+    it do
+      data = [100, 200, 200, 500]
+      actual = MovingAvg::Base.wma(data)
+      expected = (100 * 4 + 200 * 3 + 200 * 2 + 500 * 1).to_f / (4 + 3 + 2 + 1)
+      expect(actual).to eq expected
+    end
+  end
 end
