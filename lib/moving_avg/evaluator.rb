7b381011bf73f0362fda6112a576a031f031768e
kenju-wagatsuma <kenju-wagatsuma@cookpad.com>
2017-12-27 15:09:37 +0900

    impl MovingAvg::Evaluator.errors

--- lib/moving_avg/evaluator.rb
+++ lib/moving_avg/evaluator.rb
@@ -1,7 +1,20 @@
 module MovingAvg
   class Evaluator
     class << self
-      def evaluate(train_data, result, strategy)
+      def error(training_items:, teacher_data:, strategy:)
+        trained = MovingAvg::Base.public_send(strategy, training_items)
+        (trained - teacher_data).abs
+      end
+
+      def errors(training_items:, teacher_items:, window_size:, strategy:)
+        trained = MovingAvg::Helper.build_with_sliding(
+          items: training_items,
+          window_size: window_size,
+          strategy: strategy,
+        )
+        trained.zip(teacher_items).map { |trained, teacher|
+          (trained - teacher).abs
+        }
       end
     end
   end
--- spec/evaluator_spec.rb
+++ spec/evaluator_spec.rb
@@ -11,4 +11,24 @@ RSpec.describe MovingAvg::Evaluator do
       expect(actual).to eq expected
     end
   end
+
+  describe ".errors" do
+    it do
+      training_items = [100, 200, 300, 400, 500] # SMA() gonna be 200.0
+      teacher_items = [100.0, 100.0, 100.0, 100.0]
+      actual = MovingAvg::Evaluator.errors(
+        training_items: training_items,
+        teacher_items: teacher_items,
+        window_size: 2,
+        strategy: :simple_moving_average,
+      )
+      expected = [
+        MovingAvg::Base.sma([100, 200]) - teacher_items[0],
+        MovingAvg::Base.sma([200, 300]) - teacher_items[1],
+        MovingAvg::Base.sma([300, 400]) - teacher_items[2],
+        MovingAvg::Base.sma([400, 500]) - teacher_items[3],
+      ]
+      expect(actual).to eq expected
+    end
+  end
 end
